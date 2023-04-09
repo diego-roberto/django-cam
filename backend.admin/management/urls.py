f516c9397752757db77f42bb6766e6b2bccf7f19
Diego Roberto <diego-roberto@hotmail.com>
2023-04-08 23:01:17 -0300

    Correção de rotas

--- backend.admin/management/urls.py
+++ backend.admin/management/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .admin import admin_site
 from .views import DepartamentoList, DepartamentoDetail, FuncionarioList, FuncionarioDetail, ProjetoList, ProjetoDetail
 
 urlpatterns = [
+    path('admin/', admin_site.urls),
     path('departamentos/', DepartamentoList.as_view()),
     path('departamentos/<int:pk>/', DepartamentoDetail.as_view()),
     path('funcionarios/', FuncionarioList.as_view()),
